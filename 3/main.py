7e75680860da90b685edbe4bcb9fd6fe6b391d3c
John <jrobdikeman@gmail.com>
2016-05-09 18:07:37 -0500

    made a horrible solution to 3

--- 3/main.py
+++ 3/main.py
@@ -1,15 +1,23 @@
+from tqdm import tqdm
+
+def is_prime(num):
+    ind = long(2)
+    while ind < num:
+        if not num % ind:
+            return False
+        ind += 1
+    return True
+
 largest = 1
 big = 600851475143
 ind = long(1)
 
-while ind < big:
-    ind2 = long(1)
-    while ind2 < ind:
-        if not num % a:
-            break
-            break
-    if not big % num:
-        largest = num
-    ind += 1
+with tqdm(total = big) as bar:
+    while ind < big:
+        if is_prime(ind):
+            if not big % ind:
+                largest = ind
+        ind += 1
+        bar.update(1)
 
 print largest
