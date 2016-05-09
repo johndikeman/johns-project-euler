6942ba1b1293eeee7d40ff4987b9fe0b2d46eaee
John <jrobdikeman@gmail.com>
2016-05-09 12:35:14 -0500

    started working on #3

--- 2/main.py
+++ 2/main.py
@@ -1,12 +1,13 @@
 
-fin = 0
+fin = 2
 fib = [1,2]
 ind = 1
 
-while under:
+while 1:
     a = fib[ind] + fib[ind-1]
+    # print a
     if a > 4000000: break
     fib.append(a)
-    if not a % 2: fin += a
+    if a % 2 == 0: fin += a
     ind += 1
 print fin
---
+++ 3/main.py
@@ -0,0 +1,15 @@
+largest = 1
+big = 600851475143
+ind = long(1)
+
+while ind < big:
+    ind2 = long(1)
+    while ind2 < ind:
+        if not num % a:
+            break
+            break
+    if not big % num:
+        largest = num
+    ind += 1
+
+print largest
